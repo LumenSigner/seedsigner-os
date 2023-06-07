f3993872cac1bf372015d11948cf70c66f0df5fe
overcat <4catcode@gmail.com>
2023-06-07 19:24:06 +0800

    Update python-stellar-sdk-mini to 8.2.0.post3

--- opt/external-packages/python-stellar-sdk-mini/python-stellar-sdk-mini.mk
+++ opt/external-packages/python-stellar-sdk-mini/python-stellar-sdk-mini.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_STELLAR_SDK_MINI_VERSION = 8.2.0.post2
+PYTHON_STELLAR_SDK_MINI_VERSION = 8.2.0.post3
 PYTHON_STELLAR_SDK_MINI_SOURCE = stellar-sdk-mini-$(PYTHON_STELLAR_SDK_MINI_VERSION).tar.gz
-PYTHON_STELLAR_SDK_MINI_SITE = https://files.pythonhosted.org/packages/4c/7b/54ed8d6b90da16543479cbd9b249382565a126770cd037c780fcdb2b4a6c
+PYTHON_STELLAR_SDK_MINI_SITE = https://files.pythonhosted.org/packages/0e/97/34000c22474efe140e8fc276ec75ff47ce037d57bb29813768f600871f21
 PYTHON_STELLAR_SDK_MINI_SETUP_TYPE = setuptools
 PYTHON_STELLAR_SDK_MINI_LICENSE = Apache-2.0
 PYTHON_STELLAR_SDK_MINI_LICENSE_FILES = LICENSE
